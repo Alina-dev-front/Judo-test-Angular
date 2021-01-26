c95fb157136cd3c8b3c8acd1219220d9e7592914
Alina-dev-front <kolmykova93@gmail.com>
2021-01-26 17:22:17 +0100

    create main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'judo-test-project-ang';
+  title: string = 'Judo Test';
 }
---
+++ src/tests/test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    templateUrl: './test.component.html',
+    styleUrls: ['./test.component.css']
+})
+
+export class TestComponent {
+    
+}
